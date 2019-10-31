2e1346e9fcf58a41c7f30bb8dcb132423c5ca0e3
tding <tding@W-255-02>
2019-10-31 13:52:05 -0400

    test git function

---
+++ CollegeStudent.java
@@ -0,0 +1,14 @@
+public class CollegeStudent extends Student {
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    private  String degree;
+
+
+}
